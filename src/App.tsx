0ff23606c79f178951cbbc6ce06696d33b904ebf
Raghu M S <raghums1999@gmail.com>
2023-09-21 23:31:48 +0530

    constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import DataTable from "./components/DataTable";
+import { DataItem } from "./components/constants/DataItems";
 
 interface DataItem {
   Class: number;
@@ -11,40 +11,8 @@ interface DataItem {
 }
 
 function App() {
-  const data: DataItem[] = [
-    {
-      Class: 1,
-      Flavanoids: 1.5,
-      Ash: 2.5,
-      Hue: 0.6,
-      Magnesium: 40,
-      Gamma: 0.125,
-    },
-    {
-      Class: 1,
-      Flavanoids: 1.8,
-      Ash: 2.3,
-      Hue: 0.8,
-      Magnesium: 42,
-      Gamma: 0.143,
-    },
-    {
-      Class: 2,
-      Flavanoids: 2.2,
-      Ash: 2.7,
-      Hue: 0.7,
-      Magnesium: 35,
-      Gamma: 0.22,
-    },
-    {
-      Class: 2,
-      Flavanoids: 2.5,
-      Ash: 2.6,
-      Hue: 0.9,
-      Magnesium: 38,
-      Gamma: 0.122,
-    },
-  ];
+
+  const data: DataItem[] = DataItem;
 
   return (
     <div className="App">
---
+++ src/components/constants/DataItems.tsx
@@ -0,0 +1,34 @@
+export const DataItem = [
+  {
+    Class: 1,
+    Flavanoids: 1.5,
+    Ash: 2.5,
+    Hue: 0.6,
+    Magnesium: 40,
+    Gamma: 0.125,
+  },
+  {
+    Class: 1,
+    Flavanoids: 1.8,
+    Ash: 2.3,
+    Hue: 0.8,
+    Magnesium: 42,
+    Gamma: 0.143,
+  },
+  {
+    Class: 2,
+    Flavanoids: 2.2,
+    Ash: 2.7,
+    Hue: 0.7,
+    Magnesium: 35,
+    Gamma: 0.22,
+  },
+  {
+    Class: 2,
+    Flavanoids: 2.5,
+    Ash: 2.6,
+    Hue: 0.9,
+    Magnesium: 38,
+    Gamma: 0.122,
+  },
+];
